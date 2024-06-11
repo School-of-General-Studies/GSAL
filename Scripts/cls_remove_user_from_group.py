41e55ed84a28902a9f2220beef0bf7fecb4849ec
Ying <82975239+yl3627@users.noreply.github.com>
2024-06-11 13:08:13 -0400

    Update cls_remove_user_from_group.py

--- Scripts/cls_remove_user_from_group.py
+++ Scripts/cls_remove_user_from_group.py
@@ -1,50 +1,62 @@
+# Import necessary modules
 from sf_connection_uat import get_sf_connection
 from simple_salesforce import Salesforce, SalesforceMalformedRequest
-from datetime import datetime
 
-
+# Establish Salesforce connection
 sf = get_sf_connection()
 
 class SalesforceRemoveGroupManager:
     def __init__(self, group_id, user_field):
+        """
+        Initialize the SalesforceRemoveGroupManager with group ID and user field.
+        """
         self.group_id = group_id
         self.user_field = user_field
         self.sf = get_sf_connection()
         self.remove_count = 0
 
     def fetch_groupmenber(self):
-       
+        """
+        Fetch members of the group who are active users and do not meet the criteria specified by user_field.
+        """
         querySOQL = f"""
         SELECT Id
-        FROM  GroupMember
-        WHERE  GroupId = '{self.group_id}' AND  UserOrGroupId IN (
-            SELECT ID FROM User WHERE  IsActive = true And  {self.user_field} = False
+        FROM GroupMember
+        WHERE GroupId = '{self.group_id}' AND UserOrGroupId IN (
+            SELECT ID FROM User WHERE IsActive = true And {self.user_field} = False
         )
         """
-
-        print (querySOQL )
         result = self.sf.query(querySOQL)
         return result['records']
 
     def remove_users_from_group(self, groupmembers):
-        new_records = [{'ID': groupmember['Id'], 'GroupId': self.group_id} for groupmember in groupmembers]
-        if new_records:
-            print(f"Attempting to remove {len(new_records)} users.")
-            for new_record in new_records:
+        """
+        Remove the fetched group members from the specified group.
+        """
+        ids_to_delete = [{'ID': groupmember['Id'], 'GroupId': self.group_id} for groupmember in groupmembers]
+        if ids_to_delete:
+            print(f"Attempting to remove {len(ids_to_delete)} users.")
+            for id_to_delete in ids_to_delete:
                 try:
-                    res = self.sf.GroupMember.delete(new_record)
+                    res = self.sf.GroupMember.delete(id_to_delete)
                     if 'success' in res and res['success']:
-                        print(f"Record inserted successfully, Id: {res['id']}")
-                        self.insert_count += 1
+                        print(f"Record deleted successfully, Id: {res['id']}")
+                        self.remove_count += 1
                     else:
-                        print(f"Error inserting record: {res.get('errors', 'Unknown error')}")
+                        print(f"Error deleting record: {res.get('errors', 'Unknown error')}")
                 except SalesforceMalformedRequest as e:
                     print(f"An error occurred: {e}")
 
     def process_group_members_remove(self):
-        users = self.fetch_groupmenber()
-        if users:
-            self.remove_users_from_group(users)
+        """
+        Process the group members by fetching them and removing them from the group.
+        """
+        groupmembers = self.fetch_groupmenber()
+        if groupmembers:
+            self.remove_users_from_group(groupmembers)
         else:
-            print("No records to insert.")
-        print(f"Records remove successfully: {self.remove_count}")
+            print("No records to delete.")
+        print(f"Records removed successfully: {self.remove_count}")
+        return {
+            'records_Removed': self.remove_count
+        }
